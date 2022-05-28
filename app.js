1b23199acc9fa0d6883b5f24ef80f7971eeabda9
guclaw <blazejswierc@gmail.com>
2022-05-28 10:53:55 +0200

    2 commmit

---
+++ app.js
@@ -0,0 +1,37 @@
+//Variables
+
+// var name = 42;
+// -- after 2015
+// let name2 = 42
+// const name3 = 42;
+
+// Redeclaration, ta sama zmienna przypisana 2 razy
+
+// - var
+
+//error
+//let x = 10;
+//let x = 10;
+
+
+// Reinitialization/reassigment
+
+// - var
+// - let
+
+//correct
+
+// let x = 42;
+// x = 666;
+
+//error
+//const x = 42;
+// x = 42;
+
+//Hoisting
+
+console.log(x);
+var x = 42;
+
+//Scope
+
